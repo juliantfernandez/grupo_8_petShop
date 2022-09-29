56d16c4b20da1c903c5a9bbb3fd786aafcb18208
Manu3100 <m-giovinazzo@hotmail.com>
2022-09-29 18:12:15 -0300

    agrego 2 productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -15,7 +15,20 @@ let productsController = {
                 nombre: 'Grandes',
                 precio: 3500,
                 descripcion:'Comida para perros grandes',
-        }
+        },
+            {
+                id:3,
+                nombre:'Transportador',
+                precio: 15000,
+                descripcion: 'Jaula transportadora intermedia',
+
+            },
+            {
+                id:4,
+                nombre:'Litera',
+                precio:5000,
+                descripcion:'Litera sanitaria mediana',
+            }
         ]
 
         res.render('index', {p: products})
