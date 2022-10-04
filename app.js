841c6e653f085829dedb946e66f0aac61a7e21ef
juliantfernandez <juliant.fernandez0611@gmail.com>
2022-10-04 20:01:50 -0300

    configuracion para deploy

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ let productsRoutes = require('./src/routes/productsRoutes')
 app.use('/', productsRoutes)
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo')
-})
-
-
-// app.listen(process.env.PORT || 3000, function() {
-//     console.log("Servidor corriendo");
+// app.listen(3000, () => {
+//     console.log('Servidor corriendo')
 // })
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
 
 // app.get('/', (req, res) => {
 //     let htmlPath = path.join(__dirname, './views/index.html')
