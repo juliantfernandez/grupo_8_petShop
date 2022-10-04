0766354d1d94371062eb1d2c287a0913e21b18b1
juliantfernandez <juliant.fernandez0611@gmail.com>
2022-10-04 20:00:12 -0300

    correccion de vistas y ejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require('express')
 const path = require('path')
+const usersController = require('./src/controllers/usersController')
 let app = express()
 app.set('view engine', 'ejs')
 const publicPath = path.resolve(__dirname, './public')
@@ -9,14 +10,14 @@ let productsRoutes = require('./src/routes/productsRoutes')
 app.use('/', productsRoutes)
 
 
-// app.listen(3000, () => {
-//     console.log('Servidor corriendo')
-// })
-
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo");
+app.listen(3000, () => {
+    console.log('Servidor corriendo')
 })
+
+
+// app.listen(process.env.PORT || 3000, function() {
+//     console.log("Servidor corriendo");
+// })
 
 // app.get('/', (req, res) => {
 //     let htmlPath = path.join(__dirname, './views/index.html')
