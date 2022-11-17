58b5daeaa4382fb17230f2a8c092365a155b206f
juliantfernandez <juliant.fernandez0611@gmail.com>
2022-11-17 19:13:55 -0300

    test deploy

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,58 +1,58 @@
 let express = require('express');
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
-const {body} = require('express-validator')
+// const multer = require('multer');
+// const path = require('path');
+// const {body} = require('express-validator')
 let productsController = require('../controllers/productsController');
-const guestMiddleware = require('../../Middlewares/guestMiddleware')
+// const guestMiddleware = require('../../Middlewares/guestMiddleware')
 
-const configImage = multer.diskStorage({
-    destination: (req, file, cb)=> {
-        cb(null, path.join(__dirname, '../../public/img/products'))
-    },
-    filename: (req, file, cb) =>{
-        let nombreImagen = Date.now() + file.originalname;
-        cb(null, nombreImagen)
-    }
-})
+// const configImage = multer.diskStorage({
+//     destination: (req, file, cb)=> {
+//         cb(null, path.join(__dirname, '../../public/img/products'))
+//     },
+//     filename: (req, file, cb) =>{
+//         let nombreImagen = Date.now() + file.originalname;
+//         cb(null, nombreImagen)
+//     }
+// })
 
-const validationsProducts = [
-    body('nombre').notEmpty().withMessage('Debe ingresar un nombre para el producto'),
-    body('precio').notEmpty().withMessage('Debe ingresar un Precio para el producto'),
-    body('descripcion').notEmpty().withMessage('Debe ingresar una descripcion para su producto'),
-    body('image').custom((value, {req})=>{
-        let file = req.file; 
-        let acceptedExtensions = ['.jpg', '.png', '.gif'];
-        if(!file){
-            throw new Error('Debes subir una imagen')
-        }else {
-            let fileExtension = path.extname(file.originalname);
-            if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(`Las extensiones permitidas son ${acceptedExtensions.join(', ')}`);
-            }
-        }
+// const validationsProducts = [
+//     body('nombre').notEmpty().withMessage('Debe ingresar un nombre para el producto'),
+//     body('precio').notEmpty().withMessage('Debe ingresar un Precio para el producto'),
+//     body('descripcion').notEmpty().withMessage('Debe ingresar una descripcion para su producto'),
+//     body('image').custom((value, {req})=>{
+//         let file = req.file; 
+//         let acceptedExtensions = ['.jpg', '.png', '.gif'];
+//         if(!file){
+//             throw new Error('Debes subir una imagen')
+//         }else {
+//             let fileExtension = path.extname(file.originalname);
+//             if (!acceptedExtensions.includes(fileExtension)) {
+//                 throw new Error(`Las extensiones permitidas son ${acceptedExtensions.join(', ')}`);
+//             }
+//         }
 
-        return true;
-    })
-]
+//         return true;
+//     })
+// ]
 
-const uploadFile = multer({storage: configImage})
+// const uploadFile = multer({storage: configImage})
 
 router.get('/', productsController.index)
 
-router.get('/productCreate', guestMiddleware ,productsController.create)
+// router.get('/productCreate', guestMiddleware ,productsController.create)
 
-router.get('/productEdit/:id', productsController.edit)
+// router.get('/productEdit/:id', productsController.edit)
 
-router.put('/productEdit/:id', uploadFile.single('imageEdit'), productsController.update)
+// router.put('/productEdit/:id', uploadFile.single('imageEdit'), productsController.update)
 
-router.get('/productDetail', productsController.detail)
+// router.get('/productDetail', productsController.detail)
 
-router.get('/productDetail/:id', productsController.detailId)
+// router.get('/productDetail/:id', productsController.detailId)
 
-router.post('/productCreate', uploadFile.single('image'), validationsProducts ,productsController.store)
+// router.post('/productCreate', uploadFile.single('image'), validationsProducts ,productsController.store)
 
-router.delete('/delete/:id', productsController.delete)
+// router.delete('/delete/:id', productsController.delete)
 
 
 
