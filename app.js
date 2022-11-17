c8782837d18ef60d91da40636d576bb2bc0a37ab
juliantfernandez <juliant.fernandez0611@gmail.com>
2022-11-17 19:09:45 -0300

    test deploy

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let express = require('express')
 const path = require('path')
 let app = express()
-let session = require('express-session')
+// let session = require('express-session')
 const publicPath = path.resolve(__dirname, './public')
 app.set('view engine', 'ejs')
 
-const userLoggedMiddleware = require('./Middlewares/userLoggedMiddleware')
+// const userLoggedMiddleware = require('./Middlewares/userLoggedMiddleware')
 
 
-app.use(session({
-    secret: "Shhh, It's a secret",
-    resave: false,
-    saveUninitialized: false,
-}));
+// app.use(session({
+//     secret: "Shhh, It's a secret",
+//     resave: false,
+//     saveUninitialized: false,
+// }));
 app.use(userLoggedMiddleware)
 app.use(express.static(publicPath))
 app.use(express.urlencoded({extended:false})); //Captura info x POST
@@ -24,15 +24,15 @@ app.use(methodOverride('_method')); // PUT,DELETE
 let productsRoutes = require('./src/routes/productsRoutes')
 app.use('/', productsRoutes)
 
-const usersRoutes = require('./src/routes/usersRoutes')
-app.use('/', usersRoutes)
+// const usersRoutes = require('./src/routes/usersRoutes')
+// app.use('/', usersRoutes)
 
-const petshopRoutes = require('./src/routes/petshopRoutes')
-app.use('/', petshopRoutes)
+// const petshopRoutes = require('./src/routes/petshopRoutes')
+// app.use('/', petshopRoutes)
 
- app.use((req,res,next) => {
-     res.status(404).render('error')
- })
+//  app.use((req,res,next) => {
+//      res.status(404).render('error')
+//  })
 
 // app.listen(3000, () => {
 //     console.log('Servidor corriendo')
