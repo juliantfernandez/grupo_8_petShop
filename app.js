84a46605c15e4a1c964c2915ad1e51730bdef1a2
juliantfernandez <juliant.fernandez0611@gmail.com>
2022-11-17 18:18:11 -0300

    config deploy

--- app.js
+++ app.js
@@ -34,14 +34,10 @@ app.use('/', petshopRoutes)
 //     res.status(404).render('error')
 // })
 
-// app.listen(3000, () => {
-//     console.log('Servidor corriendo')
-// })
+app.listen(3000, () => {
+    console.log('Servidor corriendo')
+})
 
-const port = process.env.PORT || 4000;
-
-app.listen(port)
- console.log('listen port' + port);
 
 // app.listen(process.env.PORT || 3000, function() {
 //     console.log("Servidor corriendo");
